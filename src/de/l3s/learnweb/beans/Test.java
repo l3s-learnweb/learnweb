df36e882d5850e09a4ace2232a4070ca4659e20d
Michaela Rosebrock <rosebrock@somewhere.com>
2015-03-05 15:47:35 +0000

    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/Sandbox/LearnwebP5@3395 422a2c9f-66af-4668-9993-60dc3b0871b8

---
+++ src/de/l3s/learnweb/beans/Test.java
@@ -0,0 +1,39 @@
+package de.l3s.learnweb.beans;
+
+public class Test
+{
+    private String first;
+    private String last;
+    private String name;
+
+    public String getFirst()
+    {
+	return first;
+    }
+
+    public void setFirst(String first)
+    {
+	this.first = first;
+    }
+
+    public String getLast()
+    {
+	return last;
+    }
+
+    public void setLast(String last)
+    {
+	this.last = last;
+    }
+
+    public String getName()
+    {
+	return name;
+    }
+
+    public void setName(String name)
+    {
+	this.name = name;
+    }
+
+}
