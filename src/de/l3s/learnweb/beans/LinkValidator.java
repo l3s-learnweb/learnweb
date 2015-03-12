51719fb7114a10a29772bca897c8705fed6446a4
Michaela Rosebrock <rosebrock@somewhere.com>
2015-03-12 16:06:06 +0000

    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/Sandbox/LearnwebP5@3408 422a2c9f-66af-4668-9993-60dc3b0871b8

---
+++ src/de/l3s/learnweb/beans/LinkValidator.java
@@ -0,0 +1,13 @@
+package de.l3s.learnweb.beans;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+public class LinkValidator
+{
+    public void linkvalidator(FacesContext ctx, UIComponent component, Object value) throws ValidatorException
+    {
+
+    }
+}
