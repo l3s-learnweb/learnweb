8c25a634b8d79b5dfa130d3229bb4a6fb8ce61a4
Philipp Kemkes <kemkes@l3s.de>
2015-06-03 19:36:25 +0000

    renamed Project; refactored forumPost
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@3748 422a2c9f-66af-4668-9993-60dc3b0871b8

--- src/de/l3s/learnweb/ForumPost.java
+++ src/de/l3s/learnweb/ForumPost.java
@@ -1,10 +1,85 @@
 package de.l3s.learnweb;
 
+import java.util.Date;
+
 public class ForumPost
 {
     private int userId;
-    private String post;
-    private int topic_id;
-    private int group_id;
-    private int user_id;
+    private int topicId;
+    private int groupId;
+    private String text;
+    private Date date;
+    private int editCount;
+    private Date lastEditDate;
+
+    public int getUserId()
+    {
+	return userId;
+    }
+
+    public void setUserId(int userId)
+    {
+	this.userId = userId;
+    }
+
+    public int getTopicId()
+    {
+	return topicId;
+    }
+
+    public void setTopicId(int topicId)
+    {
+	this.topicId = topicId;
+    }
+
+    public int getGroupId()
+    {
+	return groupId;
+    }
+
+    public void setGroupId(int groupId)
+    {
+	this.groupId = groupId;
+    }
+
+    public String getText()
+    {
+	return text;
+    }
+
+    public void setText(String text)
+    {
+	this.text = text;
+    }
+
+    public Date getDate()
+    {
+	return date;
+    }
+
+    public void setDate(Date date)
+    {
+	this.date = date;
+    }
+
+    public int getEditCount()
+    {
+	return editCount;
+    }
+
+    public void setEditCount(int editCount)
+    {
+	this.editCount = editCount;
+    }
+
+    public Date getLastEditDate()
+    {
+	return lastEditDate;
+    }
+
+    public void setLastEditDate(Date lastEditDate)
+    {
+	this.lastEditDate = lastEditDate;
+    }
+
 }
