4ea6b830d3be10bc4ba0ede8dab490f6a408f2a0
Trevor Zeon Fernando <fernando@l3s.de>
2015-11-28 13:53:52 +0000

    removed website option from links page.
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@4297 422a2c9f-66af-4668-9993-60dc3b0871b8

--- WebContent/resources/group/links.js
+++ WebContent/resources/group/links.js
@@ -1,6 +1,16 @@
 function onlinkClick(element) {
 	var googleAuthObj = gapi.auth2.getAuthInstance();
-	googleAuthObj.signIn().then(function(){window.open(element.href, '_blank');onSignIn(googleAuthObj.currentUser.get());});
+	googleAuthObj.signIn().then(
+			function(success)
+			{
+				window.open(element.href, '_blank');
+				onSignIn(googleAuthObj.currentUser.get());
+			},
+			function(fail)
+			{
+				return true;
+			}
+	);
 	return false;
 }
 
@@ -12,8 +22,11 @@ function onSignIn(googleUser) {
 $(document).ready(function(){
 	gapi.load('auth2', function() {
 
-	      gapi.auth2.init({fetch_basic_profile: true,
-	          scope:'https://www.googleapis.com/auth/plus.login'}).then(
+	      gapi.auth2.init(
+	    	{
+	    		fetch_basic_profile: true,
+	    		scope:'profile'
+	    	}).then(
 	            function (){
 	              console.log('init google sign in');
 	            });
