c4792d64755febc8399f9463984f11bc31fad8fe
Alana Marques de Morais <morais@l3s.de>
2015-12-17 14:56:30 +0000

    add StudentClusterInfo in Formative Assessment
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@4321 422a2c9f-66af-4668-9993-60dc3b0871b8

---
+++ src/de/l3s/clustering/StudentClusterInfo.java
@@ -0,0 +1,84 @@
+package de.l3s.clustering;
+
+public class StudentClusterInfo
+{
+
+    private double radius;
+    private String info;
+    private String nome;
+    private String color;
+    private double cx;
+    private double cy;
+
+    public StudentClusterInfo()
+    {
+	super();
+    }
+
+    public double getRadius()
+    {
+	return radius;
+    }
+
+    public void setRadius(double radius)
+    {
+	this.radius = radius;
+    }
+
+    public String getInfo()
+    {
+	return info;
+    }
+
+    public void setInfo(String info)
+    {
+	this.info = info;
+    }
+
+    public String getNome()
+    {
+	return nome;
+    }
+
+    public void setNome(String nome)
+    {
+	this.nome = nome;
+    }
+
+    public String getColor()
+    {
+	return color;
+    }
+
+    public void setColor(String color)
+    {
+	this.color = color;
+    }
+
+    public double getCx()
+    {
+	return cx;
+    }
+
+    public void setCx(double cx)
+    {
+	this.cx = cx;
+    }
+
+    public double getCy()
+    {
+	return cy;
+    }
+
+    public void setCy(double cy)
+    {
+	this.cy = cy;
+    }
+
+    @Override
+    public String toString()
+    {
+	return "-- Name:" + this.nome + " -Info:" + this.info + " -Color:" + this.color + " -Radius:" + this.radius + " - (cx, cy):(" + this.cx + "," + this.cy + ")";
+    }
+
+}
