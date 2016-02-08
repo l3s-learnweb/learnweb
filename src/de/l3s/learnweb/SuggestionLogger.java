11932a9c41a681ef2b8d18e674129a1ad230fc13
Rishita kalyani <kalyani@l3s.de>
2016-02-08 12:41:13 +0000

    Google suggestion logger added.
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@4470 422a2c9f-66af-4668-9993-60dc3b0871b8

--- src/de/l3s/learnweb/SuggestionLogger.java
+++ src/de/l3s/learnweb/SuggestionLogger.java
@@ -1,11 +1,33 @@
 package de.l3s.learnweb;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 public class SuggestionLogger
 {
@@ -64,8 +86,7 @@ public class SuggestionLogger
 		    if(container == null) // stop method was called
 			break;
 
-		    // TODO get Google suggestions for container.query, container.market
-		    String suggestionsGoogle = "";
+		    String suggestionsGoogle = googleLogger(container.market, container.query);
 
 		    try
 		    {
@@ -91,6 +112,107 @@ public class SuggestionLogger
 	    {
 		log.fatal("Suggestion logger crashed", e);
 	    }
+	}
+
+	private String googleLogger(String market, String query)
+	{
+	    if(market.length() > 2)
+	    {
+		market = market.substring(0, 2);
+	    }
+	    String suggestorUrl = "http://suggestqueries.google.com/complete/search?output=toolbar&hl=" + market + "&q=" + query;
+	    URL queryUrl = null;
+	    try
+	    {
+		queryUrl = new URL(suggestorUrl);
+	    }
+	    catch(MalformedURLException e)
+	    {
+		log.error("Error in URL formation of Google suggest query", e);
+	    }
+
+	    HttpURLConnection connection = null;
+	    try
+	    {
+		connection = (HttpURLConnection) queryUrl.openConnection();
+	    }
+	    catch(IOException e)
+	    {
+		log.error("Error in establishing connection with Google suggest query URL", e);
+	    }
+	    try
+	    {
+		connection.setRequestMethod("GET");
+	    }
+	    catch(ProtocolException e)
+	    {
+		log.error(e);
+	    }
+	    connection.setRequestProperty("Accept", "application/xml");
+
+	    InputStream xml = null;
+	    try
+	    {
+		xml = connection.getInputStream();
+	    }
+	    catch(IOException e)
+	    {
+		log.error("IO exception in getting google suggestions in xml form", e);
+	    }
+	    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	    DocumentBuilder db = null;
+	    try
+	    {
+		db = dbf.newDocumentBuilder();
+	    }
+	    catch(ParserConfigurationException e)
+	    {
+		log.error(e);
+	    }
+	    Document doc = null;
+	    try
+	    {
+		doc = db.parse(xml);
+	    }
+	    catch(SAXException e)
+	    {
+		log.error("Exception in parsing xml doc of google suggestion", e);
+	    }
+	    catch(IOException e)
+	    {
+		log.error("Exception in parsing xml doc of google suggestion", e);
+	    }
+	    TransformerFactory tf = TransformerFactory.newInstance();
+	    Transformer transformer = null;
+	    try
+	    {
+		transformer = tf.newTransformer();
+	    }
+	    catch(TransformerConfigurationException e2)
+	    {
+		log.error(e2);
+	    }
+	    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+	    StringWriter writer = new StringWriter();
+	    try
+	    {
+		transformer.transform(new DOMSource(doc), new StreamResult(writer));
+	    }
+	    catch(TransformerException e1)
+	    {
+		log.error(e1);
+	    }
+	    String output = writer.getBuffer().toString().trim();
+	    ArrayList<String> queries = new ArrayList<String>();
+	    while(output.contains("=\""))
+	    {
+		int subStringIndex = output.indexOf("=\"", 0);
+		output = output.substring(subStringIndex + 2, output.length() - 1);
+
+		queries.add(output.substring(0, output.indexOf("\"")));
+	    }
+	    String suggestion = StringUtils.join(queries).replaceAll("\\[|\\]", "");
+	    return suggestion;
 	}
     }
 
