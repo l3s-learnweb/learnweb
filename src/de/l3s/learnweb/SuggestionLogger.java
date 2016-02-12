be3c3c7180da2001f6e46c02812b619938bcc933
Rishita kalyani <kalyani@l3s.de>
2016-02-12 10:33:59 +0000

    Better exception handling. Cleaned the code a bit.
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@4478 422a2c9f-66af-4668-9993-60dc3b0871b8

--- src/de/l3s/learnweb/SuggestionLogger.java
+++ src/de/l3s/learnweb/SuggestionLogger.java
@@ -2,7 +2,6 @@ package de.l3s.learnweb;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -10,25 +9,17 @@ import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
-import de.l3s.util.StringHelper;
 
 public class SuggestionLogger
 {
@@ -126,106 +117,91 @@ public class SuggestionLogger
 	    }
 	    String suggestorUrl = "http://suggestqueries.google.com/complete/search?output=toolbar&hl=" + market + "&q=" + query;
 	    URL queryUrl = null;
+	    try
+	    {
+		try
+		{
+		    queryUrl = new URL(suggestorUrl);
+		}
+		catch(MalformedURLException e)
+		{
+		    log.error("Error in URL formation of Google suggest query", e);
+		    return "";
+		}
 
-	    try
-	    {
-		queryUrl = new URL(suggestorUrl);
-	    }
-	    catch(MalformedURLException e)
-	    {
-		log.error("Error in URL formation of Google suggest query", e);
-		return "";
-	    }
+		HttpURLConnection connection = null;
+		try
+		{
+		    connection = (HttpURLConnection) queryUrl.openConnection();
+		}
+		catch(IOException e)
+		{
+		    log.error("Error in establishing connection with Google suggest query URL", e);
+		    return "";
+		}
+		try
+		{
+		    connection.setRequestMethod("GET");
+		}
+		catch(ProtocolException e)
+		{
+		    log.error(e);
+		    return "";
+		}
+		connection.setRequestProperty("Accept", "application/xml");
 
-	    HttpURLConnection connection = null;
-	    try
-	    {
-		connection = (HttpURLConnection) queryUrl.openConnection();
-	    }
-	    catch(IOException e)
-	    {
-		log.error("Error in establishing connection with Google suggest query URL", e);
-		return "";
-	    }
-	    try
-	    {
-		connection.setRequestMethod("GET");
-	    }
-	    catch(ProtocolException e)
-	    {
-		log.error(e);
-		return "";
-	    }
-	    connection.setRequestProperty("Accept", "application/xml");
+		InputStream xml = null;
+		try
+		{
+		    xml = connection.getInputStream();
+		}
+		catch(IOException e)
+		{
+		    log.error("IO exception in getting google suggestions in xml form", e);
+		    return "";
+		}
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = null;
+		try
+		{
+		    db = dbf.newDocumentBuilder();
+		}
+		catch(ParserConfigurationException e)
+		{
+		    log.error(e);
+		    return "";
+		}
+		Document doc = null;
+		try
+		{
+		    try
+		    {
+			doc = db.parse(xml, "UTF-8");
+		    }
+		    catch(IOException e)
+		    {
+			log.error(e);
+		    }
+		}
+		catch(SAXException e)
+		{
+		    log.error("Exception in parsing xml doc of google suggestion", e);
+		}
+		NodeList getData = doc.getElementsByTagName("suggestion");
+		for(int i = 0; i < getData.getLength(); i++)
+		{
+		    Node currentSuggestion = getData.item(i);
 
-	    InputStream xml = null;
-	    try
-	    {
-		xml = connection.getInputStream();
+		    suggestion += currentSuggestion.getAttributes().getNamedItem("data").getNodeValue();
+		    suggestion += ", ";
+		}
+		suggestion.substring(0, suggestion.lastIndexOf(","));
 	    }
-	    catch(IOException e)
+	    catch(NullPointerException e)
 	    {
-		log.error("IO exception in getting google suggestions in xml form", e);
+		log.error("Google suggestions not retrieved", e);
 		return "";
 	    }
-	    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-	    DocumentBuilder db = null;
-	    try
-	    {
-		db = dbf.newDocumentBuilder();
-	    }
-	    catch(ParserConfigurationException e)
-	    {
-		log.error(e);
-		return "";
-	    }
-	    Document doc = null;
-	    try
-	    {
-		doc = db.parse(xml);
-	    }
-	    catch(IOException | SAXException e)
-	    {
-		log.error("Exception in parsing xml doc of google suggestion", e);
-		return "";
-	    }
-
-	    TransformerFactory tf = TransformerFactory.newInstance();
-	    Transformer transformer = null;
-	    try
-	    {
-		transformer = tf.newTransformer();
-	    }
-	    catch(TransformerConfigurationException e2)
-	    {
-		log.error(e2);
-		return "";
-	    }
-	    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-	    StringWriter writer = new StringWriter();
-	    try
-	    {
-		transformer.transform(new DOMSource(doc), new StreamResult(writer));
-	    }
-	    catch(TransformerException e1)
-	    {
-		log.error(e1);
-		return "";
-	    }
-	    String output = writer.getBuffer().toString().trim();
-
-	    ArrayList<String> queries = new ArrayList<String>();
-	    while(output.contains("=\""))
-	    {
-		int subStringIndex = output.indexOf("=\"", 0);
-		output = output.substring(subStringIndex + 2, output.length() - 1);
-
-		queries.add(output.substring(0, output.indexOf("\"")));
-	    }
-	    suggestion = StringHelper.implode(queries, ",").replaceAll("\\[|\\]", "");
-
-	    // replaced to get rid of eclipse warning
-	    //suggestion = StringUtils.join(queries).replaceAll("\\[|\\]", "");
 
 	    return suggestion;
 	}
