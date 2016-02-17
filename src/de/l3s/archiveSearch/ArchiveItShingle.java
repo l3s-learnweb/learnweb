350f04ebe2fc646faa5120c64e69edfe371f3950
Prasad Abhinav <abhinav@l3s.de>
2016-02-17 14:16:13 +0000

    Added functionality to Create set of unique web archives of a resource using shingles.
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@4492 422a2c9f-66af-4668-9993-60dc3b0871b8

---
+++ src/de/l3s/archiveSearch/ArchiveItShingle.java
@@ -0,0 +1,129 @@
+package de.l3s.archiveSearch;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import de.l3s.learnweb.ArchiveUrl;
+import de.l3s.learnweb.MementoClient;
+import de.l3s.util.Image;
+import de.l3s.util.StringHelper;
+
+public class ArchiveItShingle
+{
+    private final MementoClient mementoclient;
+
+    private final int w = 10; // the N-gram dimension i.e w words in a shingle
+
+    public ArchiveItShingle()
+    {
+	mementoclient = new MementoClient(null);
+    }
+
+    private final Set<String> intersect = new HashSet<String>();
+    private final Set<String> union = new HashSet<String>();
+
+    public float computeIndex(Set<String> set1, Set<String> set2)
+    {
+	intersect.clear();
+	intersect.addAll(set1);
+	intersect.retainAll(set2);
+	union.clear();
+	union.addAll(set1);
+	union.addAll(set2);
+	return (float) intersect.size() / union.size();
+    }
+
+    public void processWebsite(String url, String timestamp) throws IOException
+    {
+	URL thumbnailUrl = new URL("http://prometheus.kbs.uni-hannover.de/thumbnail/thumb_wb.php?url=" + StringHelper.urlEncode(url));
+	// Process the Image
+	Image img = new Image(thumbnailUrl.openStream());
+	//java.io.File is the actual class File for creating files
+	java.io.File actualFile = new java.io.File("F://DevTools//Work stuff//Crawler//Thumbnails//", timestamp + ".png");
+	// copy the data into the file
+	OutputStream outputStream = new FileOutputStream(actualFile);
+	IOUtils.copy(img.getInputStream(), outputStream);
+	outputStream.close();
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+	String url = null;
+	float d = 0;
+
+	ArchiveItShingle archiveItShingle = new ArchiveItShingle();
+
+	List<ArchiveUrl> listOfArchives = new LinkedList<ArchiveUrl>();
+	listOfArchives = archiveItShingle.mementoclient.getArchiveItVersions(227, "http://www.conservateur.ca/");
+	List<String> wordList = new ArrayList<String>();
+	List<String> shingleList = new LinkedList<String>();
+	List<Float> Jaccard = new ArrayList<Float>();
+
+	Set<String> setOfShingles = new HashSet<String>();
+	Set<String> setOfNearUniqueArchives = new HashSet<String>();
+	Set<String> setOfNonUniqueUrls = new HashSet<String>();
+
+	HashMap<String, Set<String>> hashmap = new HashMap<String, Set<String>>();
+	for(ArchiveUrl archiveUrl : listOfArchives)
+	{
+	    setOfShingles.clear();
+	    url = archiveUrl.getArchiveUrl();
+	    Document document = Jsoup.connect(url).get(); //fetch the web pages
+	    document.select("wb_div#wm-disclaim").remove(); //remove Archive disclaimer from html text
+	    Elements element = document.select("body").first().children();
+	    String[] words = element.text().replaceAll("[!?,.]", "").split(" ");
+	    wordList.addAll(Arrays.asList(words));
+	    for(int i = 0; i < wordList.size() - archiveItShingle.w; i++)
+	    {
+		shingleList = wordList.subList(i, i + archiveItShingle.w);
+		setOfShingles.add(StringUtils.join(shingleList, " ").toLowerCase());
+	    }
+	    hashmap.put(url, new HashSet<>(setOfShingles));
+	}
+	url = null;
+	for(Map.Entry<String, Set<String>> entry1 : hashmap.entrySet())
+	{
+	    for(Map.Entry<String, Set<String>> entry2 : hashmap.entrySet())
+	    {
+		if(entry1 != entry2 && !setOfNonUniqueUrls.contains(entry2.getKey()))
+		{
+		    if(setOfNearUniqueArchives.contains(entry2.getKey()))
+		    {
+
+		    }
+		    else
+		    {
+			d = archiveItShingle.computeIndex(entry1.getValue(), entry2.getValue());
+			Jaccard.add(d);
+			if(d <= 0.5)
+			{
+			    setOfNearUniqueArchives.add(entry1.getKey());
+			    setOfNearUniqueArchives.add(entry2.getKey());
+			}
+		    }
+		}
+	    }
+	    setOfNonUniqueUrls.add(entry1.getKey());
+	}
+	d = 0;
+	for(String str : setOfNearUniqueArchives)
+	    archiveItShingle.processWebsite(str, Float.toString(++d));
+
+    }
+}
