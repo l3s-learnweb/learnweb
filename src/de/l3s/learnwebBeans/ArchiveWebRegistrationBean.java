27f993ca8f76ea2f9f8febffe02d928617c7351d
Trevor Zeon Fernando <fernando@l3s.de>
2016-03-24 15:47:05 +0000

    archiveweb index registration form
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@4549 422a2c9f-66af-4668-9993-60dc3b0871b8

---
+++ src/de/l3s/learnwebBeans/ArchiveWebRegistrationBean.java
@@ -0,0 +1,104 @@
+package de.l3s.learnwebBeans;
+
+import java.io.Serializable;
+import java.util.Properties;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import de.l3s.learnwebBeans.PasswordBean.GMailAuthenticator;
+
+@ManagedBean
+@RequestScoped
+public class ArchiveWebRegistrationBean extends ApplicationBean implements Serializable
+{
+    private static final long serialVersionUID = 1506604946829332647L;
+
+    @Size(min = 2, max = 50)
+    private String username;
+
+    @NotEmpty
+    private String password;
+
+    @Email
+    private String email;
+
+    public String getUsername()
+    {
+	return username;
+    }
+
+    public void setUsername(String username)
+    {
+	this.username = username;
+    }
+
+    public String getPassword()
+    {
+	return password;
+    }
+
+    public void setPassword(String password)
+    {
+	this.password = password;
+    }
+
+    public String getEmail()
+    {
+	return email;
+    }
+
+    public void setEmail(String email)
+    {
+	this.email = email;
+    }
+
+    public void sendMail()
+    {
+	String to = "fernando@l3s.de";
+
+	Properties props = new Properties();
+	props.put("mail.smtp.host", "smtp.gmail.com");
+	props.put("mail.smtp.socketFactory.port", "465");
+	props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+	props.put("mail.smtp.auth", "true");
+	props.put("mail.smtp.port", "465");
+
+	Session session = Session.getDefaultInstance(props, new GMailAuthenticator("interweb9@googlemail.com", "QDsG}GM5"));
+
+	try
+	{
+
+	    MimeMessage message = new MimeMessage(session);
+	    message.setFrom(new InternetAddress("interweb9@googlemail.com"));
+	    message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+	    message.setSubject("ArchiveWeb Registration");
+	    message.setText("username: " + username + "\npassword: " + password + "\nemail:" + email);
+	    Transport.send(message);
+	}
+	catch(MessagingException mex)
+	{
+	    addFatalMessage(mex);
+	}
+	clearForm();
+	addMessage(FacesMessage.SEVERITY_INFO, "Email Request Sent Successfully");
+    }
+
+    public void clearForm()
+    {
+	username = null;
+	password = null;
+	email = null;
+    }
+}
