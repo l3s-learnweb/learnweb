33463c8d4ffe33ec96625fb8537962e0e25f4b05
Nils Zolitschka <zolitschk@somewhere.com>
2016-06-28 10:36:33 +0000

    factsheet page for testings
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@4664 422a2c9f-66af-4668-9993-60dc3b0871b8

---
+++ src/de/l3s/learnweb/beans/FactSheetBean.java
@@ -0,0 +1,22 @@
+package de.l3s.learnweb.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class FactSheetBean
+{
+    String search;
+
+    public String getSearch()
+    {
+	return search;
+    }
+
+    public void setSearch(String search)
+    {
+	this.search = search;
+    }
+
+}
