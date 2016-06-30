cc64a1c51a6f5a25d9836ebdce4c8c00ca5995c9
Rishita kalyani <kalyani@l3s.de>
2016-06-30 20:22:26 +0000

    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@4678 422a2c9f-66af-4668-9993-60dc3b0871b8

--- WebContent/resources/js/guide.js
+++ WebContent/resources/js/guide.js
@@ -7,54 +7,9 @@ Include only methods which are required on every page.
  * Method required for the search field
  */
 var lightboxActiveResource = null;
-function removeViewstate(searchForm)
-{
-	$(searchForm).find("[name='javax.faces.ViewState']").remove();
-}
 
-function footerToggle()
-{
-	var footer = $('#footer');
-	
-	if(footer.css('display') == 'none')
-		setPreference('helptext_hide', 'false');
-	else
-		setPreference('helptext_hide', 'true');
-	
-	footer.slideToggle();
 
-	return false;
-}
 
-function footerClose()
-{
-	$('#footer').hide();
-	setPreference('helptext_hide', 'true');
-	return false;
-}
-
-var defaultVisible = 8;
-
-function updateCarousel() {
-	$('.filter-vCarousel').each(function () {
-		var $wrapper = $(this);
-		var $container = $($wrapper.find('.vCarousel-container'));
-		var height = $(":first-child", $container).outerHeight(true);
-		
-		var totalRecords = $container.children().length;
-        if (defaultVisible < totalRecords) {
-        	$wrapper.addClass('vCarousel-small');
-        	$container.css({ "max-height" : (defaultVisible - 1)*height });
-        	
-            $($wrapper.find('.vCarousel-expand')).on('click', function(e) {
-            	e.preventDefault();
-            	$wrapper.addClass('vCarousel-expanded');
-            	$container.css({ "max-height" : defaultVisible*height });
-                return false;
-            });
-        }
-	});
-}
 
 var box;
 
@@ -131,60 +86,13 @@ function lightbox_open()
 	box.show();
 };
 
-// store preferences in user account settings
-function setPreference(prefKey, prefValue)
-{
-	setPreferenceRemote([{name:'key', value:prefKey}, {name:'value', value:prefValue}]);
-}
 
-var myMarket = "en-US";
+
+
 
 $(document).ready(function()
 {	
 	lightbox_load();
-	$("#group_menu > .panelmenu").each(function( index ) 
-	{
-		var group = $(this);
-		var links = group.children('div');
-		
-		group.find('h3 .iconarrow').click(function()
-		{
-			links.slideToggle();
-			group.toggleClass('active');
-			return true;
-		});		
-	});
-
-	// initialize search field auto completion
-	if(typeof market != 'undefined')
-	{
-		if(market == "de")
-			myMarket = "de-DE";
-		else if(market == "pt")
-			myMarket = "pt-BR";
-		else if(market == "it")
-			myMarket = "it-IT";
-	}
 	
-	$("#searchfield").autocomplete({
-        source: function (request, response) {
-            $.ajax({
-                url: "http://api.bing.com/osjson.aspx?Query=" + encodeURIComponent(request.term) + "&Market="+ myMarket +"&JsonType=callback&JsonCallback=?",
-                dataType: "jsonp",
-    
-                success: function (data) {
-                    var suggestions = [];
-                    $.each(data[1], function (i, val) {
-                        suggestions.push(val);
-                    });
-                    response(suggestions);
-               
-                    var logQuerySuggestionAsync = function() {
-                    	logQuerySuggestion([{name:'query', value:request.term},{name:'suggestions', value:suggestions},{name:'market', value:myMarket}]);
-                    };
-                    setTimeout(logQuerySuggestionAsync, 0);
-                }
-            });
-        }
-    });
+	
 });
