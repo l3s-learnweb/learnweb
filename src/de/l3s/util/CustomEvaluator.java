5338ef14cec86f517527b463678d3d90b38eff71
Philipp Kemkes <kemkes@l3s.de>
2016-07-04 10:19:05 +0000

    Disabled email logger on Localhost
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@4689 422a2c9f-66af-4668-9993-60dc3b0871b8

--- src/de/l3s/util/CustomEvaluator.java
+++ src/de/l3s/util/CustomEvaluator.java
@@ -3,21 +3,47 @@ package de.l3s.util;
 import org.apache.log4j.spi.LoggingEvent;
 import org.apache.log4j.spi.TriggeringEventEvaluator;
 
-public class CustomEvaluator implements TriggeringEventEvaluator{
+import de.l3s.learnweb.Learnweb;
 
-	private int cycleSeconds = 360000;  	// Sending a group of errors if the last check was more than an hour ago
-	private long lastCheck;
-	
-	public boolean isTriggeringEvent(LoggingEvent event) { 
-		long now = System.currentTimeMillis();
-		if(now - lastCheck < cycleSeconds){
-			return false;
-		}
-		else
-		{
-			lastCheck = now;
-			return true;
-		}
+public class CustomEvaluator implements TriggeringEventEvaluator
+{
+    private long cycleSeconds = 360000L; // Sending a group of errors if the last check was more than an hour ago
+    private long lastCheck = 0L;
+    private Boolean isRunningOnLocalhost = null;
+
+    @Override
+    public boolean isTriggeringEvent(LoggingEvent event)
+    {
+	if(isRunningOnLocalhost())
+	    return false;
+
+	long now = System.currentTimeMillis();
+	if(now - lastCheck < cycleSeconds)
+	{
+	    return false;
 	}
+	else
+	{
+	    lastCheck = now;
+	    return true;
+	}
+    }
+
+    private boolean isRunningOnLocalhost()
+    {
+	if(isRunningOnLocalhost == null) // called the first time
+	{
+	    lastCheck = System.currentTimeMillis();
+
+	    String contextUrl = Learnweb.getInstance().getContextUrl();
+
+	    if(contextUrl.startsWith("http://localhost") || contextUrl.startsWith("http://learnweb.dev"))
+		isRunningOnLocalhost = true;
+
+	    return true; // true, to skip first error message
+	}
+
+	return isRunningOnLocalhost;
+    }
 
 }
