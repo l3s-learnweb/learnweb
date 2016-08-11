b3df42f9f58e2359709a6601495519694b782598
Jiawei Zhang <zhang@l3s.de>
2016-08-11 13:16:19 +0000

    for Entity object, set default English results for some given language, track places from city to country
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@4759 422a2c9f-66af-4668-9993-60dc3b0871b8

--- src/de/l3s/learnweb/facts/TestWiki.java
+++ src/de/l3s/learnweb/facts/TestWiki.java
@@ -3,18 +3,24 @@ package de.l3s.learnweb.facts;
 import java.io.IOException;
 import java.text.ParseException;
 
-import com.google.gson.Gson;;
+import org.json.JSONObject;
 
 public class TestWiki
 {
     public static void main(String[] args) throws IOException, ParseException
     {
 	System.out.println("hello");
-	Entity entity = Search.searchRdfWikidata("Q76", "en");
-	Gson gson = new Gson();
-	String json = gson.toJson(entity);
-	System.out.println(json);
+	Entity entity = Search.searchRdfWikidata("Q91", "en");
+	JSONObject json = new JSONObject(entity);
+	System.out.println(json.toString());
 	System.out.println("bye");
+	//	List<String> id = Search.searchWikiIdSparql("Barack Obama");
+	//	//	List<String> id = Search.searchWikiIdPhp("Barack Obama");
+	//	for(String idName : id)
+	//	{
+	//	    System.out.println(idName);
+	//	}
+
     }
 
 }
