cffeab28b381ebac4512eba30fc68a9552f42801
Chloe Hhwang <hhwang@somewhere.com>
2017-11-14 23:40:07 +0000

    a bean to be used for extended metadata search for resources_yell.xhtml
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@5318 422a2c9f-66af-4668-9993-60dc3b0871b8

--- src/de/l3s/learnweb/rm/beans/ExtendedMetadataSearchBean.java
+++ src/de/l3s/learnweb/rm/beans/ExtendedMetadataSearchBean.java
@@ -1,13 +1,20 @@
 package de.l3s.learnweb.rm.beans;
 
 import java.io.Serializable;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
+import de.l3s.learnweb.AbstractPaginator;
 import de.l3s.learnweb.Learnweb;
 import de.l3s.learnweb.Resource;
+import de.l3s.learnweb.ResourceDecorator;
+import de.l3s.learnweb.ResourceManager;
+import de.l3s.learnweb.User;
 import de.l3s.learnweb.beans.ApplicationBean;
-import de.l3s.learnweb.rm.ExtendedMetadataManager;
 import de.l3s.learnweb.rm.ExtendedMetadataSearchFilters;
 
 public class ExtendedMetadataSearchBean extends ApplicationBean implements Serializable
@@ -18,206 +25,473 @@ public class ExtendedMetadataSearchBean extends ApplicationBean implements Seria
     //set selected values for each metadata search filter 
 
     private static final long serialVersionUID = 3635253409650813764L;
-    private String[] selectedAuthors;
-    private String[] selectedMtypes;
-    private String[] selectedSources;
-    private String[] selectedTargets;
-    private String[] selectedPurposes;
-    private String[] selectedLanguages;
-    private String[] selectedLevels;
-
-    private List<String> authors;
-    private List<String> mtypes;
-    private List<String> sources;
-    private List<String> targets;
-    private List<String> purposes;
-    private List<String> langs;
-    private List<String> levels;
+    private final static Logger log = Logger.getLogger(ExtendedMetadataSearchBean.class);
 
     private ExtendedMetadataSearchFilters mFilters = new ExtendedMetadataSearchFilters();
+    private FilterPaginator filterPaginator;
 
-    public ExtendedMetadataSearchBean()
+    private Integer resultsPerPage = 8;
+    private Integer resultsPerGroup = 2;
+
+    private String sorting;
+    private String groupField = "";
+
+    protected long totalResults = -1;
+    private int userId;
+    private List<ResourceDecorator> results = new LinkedList<ResourceDecorator>();
+
+    public ExtendedMetadataSearchBean(User user)
     {
+
+        this.userId = user == null ? 0 : user.getId();
 
     }
 
-    public ExtendedMetadataSearchFilters populateFilters(List<Resource> gResources)
+    public FilterPaginator getFilterResults(int groupId, int folderId, ExtendedMetadataSearchFilters emFilters, User user)
     {
-        ExtendedMetadataManager mm = Learnweb.getInstance().getExtendedMetadataManager();
-        authors = new ArrayList<String>();
+        FilterPaginator fPaginator = new FilterPaginator(this);
 
-        //get authors from the given resources 
+        List<Resource> filterResults = new ArrayList<Resource>();
 
-        mFilters.setAuthors(authors);
+        //first get resources by groupId
+        filterResults = filterResultsByGroupId(groupId);
 
-        //mtype filter values (fixed values) 
-        mtypes = new ArrayList<String>();
-        mtypes.add("Text");
-        mtypes.add("Video");
-        mtypes.add("Image");
-        mtypes.add("Game");
-        mtypes.add("App");
+        //filter by author if not null
+        if(emFilters.getFilterAuthors().length > 0)
+        {
+            filterResults = filterResultsByAuthors(emFilters.getFilterAuthors(), filterResults);
+        }
 
-        //source filter values (must get it from db) 
-        sources = new ArrayList<String>();
-        sources.add("BBC");
-        sources.add("Cambridge");
-        sources.add("TED");
-        sources.add("Guardian");
+        //filter by language if not null
+        if(emFilters.getFilterLangs().length > 0)
+        {
+            filterResults = filterResultsByLanguages(emFilters.getFilterLangs(), filterResults);
 
-        //target learner values (fixed values) 
-        targets = new ArrayList<String>();
-        targets.add("Teachers");
-        targets.add("Adults");
-        targets.add("Teenagers");
-        targets.add("Young learners (Elementary)");
-        targets.add("Very young learners (Pre-school");
+        }
 
-        //purposes values (fixed values) 
-        purposes = new ArrayList<String>();
-        purposes.add("Listening Skills");
-        purposes.add("Reading Skills");
-        purposes.add("Writing Skills");
-        purposes.add("Speaking Skills");
-        purposes.add("Class Activities");
-        purposes.add("Lesson Plans");
-        purposes.add("Cross-curricular resources / CLIL");
-        purposes.add("Story-telling");
-        purposes.add("Plurallingualism");
-        purposes.add("Special Learning Needs");
-        purposes.add("Assessment");
-        purposes.add("Teacher Education Resources");
-        purposes.add("Other");
+        //filter by level if not null
+        if(emFilters.getFilterLevels().length > 0)
+        {
+            try
+            {
+                filterResults = filterResultsByLevels(emFilters.getFilterLevels(), filterResults);
+            }
+            catch(SQLException e)
+            {
+                // TODO Auto-generated catch block
+                log.debug("filtering by levels failed");
+            }
+        }
 
-        //languages values (fixed values) 
-        langs = new ArrayList<String>();
-        langs.add("English");
-        langs.add("Italian");
-        langs.add("German");
+        //filter by media source if not null
+        if((emFilters.getFilterSources().length > 0) && (emFilters.getFilterSources() != null))
+        {
+            filterResults = filterResultsByMsources(emFilters.getFilterSources(), filterResults);
+        }
 
-        //language level values (fixed values) 
-        levels = new ArrayList<String>();
-        levels.add("C2");
-        levels.add("C1");
-        levels.add("B2");
-        levels.add("B1");
-        levels.add("A2");
-        levels.add("A1");
+        //filter by media type if not null
+        if(emFilters.getFilterMtypes().length > 0)
 
+        {
+            log.info("inside filter if condition before calling filterresultsbymtypes: " + filterResults.size());
+            filterResults = filterResultsByMtypes(emFilters.getFilterMtypes(), filterResults);
+        }
+
+        //filter by audience if not null
+        if(emFilters.getFilterTargets().length > 0)
+        {
+            try
+            {
+                filterResults = filterResultsByTargets(emFilters.getFilterTargets(), filterResults);
+            }
+            catch(SQLException e)
+            {
+                // TODO Auto-generated catch block
+                log.debug("filtering by targets failed");
+            }
+        }
+
+        //filter by purpose if not null
+        if(emFilters.getFilterPurposes().length > 0)
+        {
+            try
+            {
+                filterResults = filterResultsByPurposes(emFilters.getFilterPurposes(), filterResults);
+            }
+            catch(SQLException e)
+            {
+                // TODO Auto-generated catch block
+                log.debug("filtering by purposes failed");
+            }
+        }
+
+        log.info("before setting paginator, the final result size is" + filterResults.size());
+        //convert the final filterResults into list of resource decorator
+        setTotalResults(filterResults.size());
+        this.results = convertToFinalResults(filterResults);
+
+        return fPaginator;
+
+    }
+
+    public List<ResourceDecorator> convertToFinalResults(List<Resource> fResults)
+    {
+        //we change the list of resources to list of resource decorator... 
+        List<ResourceDecorator> temp = new LinkedList<ResourceDecorator>();
+        for(int i = 0; i < fResults.size(); i++)
+        {
+            ResourceDecorator rd = new ResourceDecorator(fResults.get(i));
+            temp.add(rd);
+        }
+
+        return temp;
+    }
+
+    public List<Resource> filterResultsByGroupId(int groupId)
+    {
+        ResourceManager rsm = Learnweb.getInstance().getResourceManager();
+        List<Resource> fResults = new ArrayList<Resource>();
+        if(groupId > 0)
+        {
+            try
+            {
+                fResults = rsm.getResourcesByGroupId(groupId);
+            }
+            catch(SQLException e)
+            {
+                // TODO Auto-generated catch block
+                log.fatal("getting resources by given groupId failed: " + groupId);
+            }
+        }
+        return fResults;
+    }
+
+    public List<Resource> filterResultsByAuthors(String[] authors, List<Resource> workingCopy)
+    {
+        List<Resource> fResults = new ArrayList<Resource>();
+
+        for(int i = 0; i < workingCopy.size(); i++)
+        {
+            Resource r = workingCopy.get(i);
+
+            for(int j = 0; j < authors.length; j++)
+            {
+                if(r.getAuthor() != null)
+                {
+                    if(r.getAuthor().equalsIgnoreCase(authors[j].toLowerCase()))
+                    {
+                        fResults.add(r);
+                    }
+                }
+            }
+        }
+        return fResults;
+    }
+
+    public List<Resource> filterResultsByLanguages(String[] langs, List<Resource> workingCopy)
+    {
+        List<Resource> fResults = new ArrayList<Resource>();
+
+        for(int i = 0; i < workingCopy.size(); i++)
+        {
+            Resource r = workingCopy.get(i);
+
+            for(int j = 0; j < langs.length; j++)
+            {
+                if(r.getLanguage() != null)
+                {
+                    if(r.getLanguage().equalsIgnoreCase(langs[j]))
+                    {
+                        fResults.add(r);
+                    }
+                }
+            }
+        }
+
+        return fResults;
+    }
+
+    public List<Resource> filterResultsByMsources(String[] msources, List<Resource> workingCopy)
+    {
+        List<Resource> fResults = new ArrayList<Resource>();
+
+        for(int i = 0; i < workingCopy.size(); i++)
+        {
+            Resource r = workingCopy.get(i);
+
+            for(int j = 0; j < msources.length; j++)
+            {
+                if(r.getMsource() != null)
+                {
+                    if(r.getMsource().equalsIgnoreCase(msources[j].toLowerCase()))
+                    {
+                        fResults.add(r);
+                    }
+                }
+            }
+        }
+        return fResults;
+    }
+
+    public List<Resource> filterResultsByMtypes(String[] mtypes, List<Resource> workingCopy)
+    {
+        List<Resource> fResults = new ArrayList<Resource>();
+
+        for(int i = 0; i < workingCopy.size(); i++)
+        {
+            Resource r = workingCopy.get(i);
+            for(int j = 0; j < mtypes.length; j++)
+            {
+                if(r.getMtype() != null)
+                {
+                    String[] rtypes = r.getMtype().split(",");
+                    for(int k = 0; k < rtypes.length; k++)
+                    {
+                        if(rtypes[k].trim().equalsIgnoreCase(mtypes[j].toLowerCase()))
+                        {
+                            fResults.add(r);
+                        }
+                    }
+                }
+            }
+        }
+        return fResults;
+    }
+
+    public List<Resource> filterResultsByLevels(String[] levels, List<Resource> workingCopy) throws SQLException
+    {
+        List<Resource> fResults = new ArrayList<Resource>();
+
+        for(int i = 0; i < workingCopy.size(); i++)
+        {
+            Resource r = workingCopy.get(i);
+
+            for(int j = 0; j < levels.length; j++)
+            {
+                List<String> rlevels;
+                if(r.getExtendedMetadata().getLevels() != null)
+                {
+                    rlevels = r.getExtendedMetadata().getLevels();
+                    for(int k = 0; k < rlevels.size(); k++)
+                    {
+                        if(rlevels.get(k).equalsIgnoreCase(levels[j]))
+                        {
+                            fResults.add(r);
+                        }
+                    }
+                }
+            }
+        }
+        return fResults;
+    }
+
+    public List<Resource> filterResultsByTargets(String[] targets, List<Resource> workingCopy) throws SQLException
+    {
+        List<Resource> fResults = new ArrayList<Resource>();
+
+        for(int i = 0; i < workingCopy.size(); i++)
+        {
+            Resource r = workingCopy.get(i);
+
+            for(int j = 0; j < targets.length; j++)
+            {
+                List<String> rtargets;
+                if(r.getExtendedMetadata().getTargets() != null)
+                {
+                    rtargets = r.getExtendedMetadata().getTargets();
+                    for(int k = 0; k < rtargets.size(); k++)
+                    {
+                        if(rtargets.get(k).equalsIgnoreCase(targets[j].toLowerCase()))
+                        {
+                            fResults.add(r);
+                        }
+                    }
+                }
+            }
+        }
+        return fResults;
+    }
+
+    public List<Resource> filterResultsByPurposes(String[] purposes, List<Resource> workingCopy) throws SQLException
+    {
+        List<Resource> fResults = new ArrayList<Resource>();
+
+        for(int i = 0; i < workingCopy.size(); i++)
+        {
+            Resource r = workingCopy.get(i);
+
+            for(int j = 0; j < purposes.length; j++)
+            {
+                List<String> rpurposes;
+                if(r.getExtendedMetadata().getPurposes() != null)
+                {
+                    rpurposes = r.getExtendedMetadata().getPurposes();
+                    for(int k = 0; k < rpurposes.size(); k++)
+                    {
+                        if(rpurposes.get(k).equalsIgnoreCase(purposes[j].toLowerCase()))
+                        {
+                            fResults.add(r);
+                        }
+                    }
+                }
+            }
+        }
+        return fResults;
+    }
+
+    public long getTotalResultCount()
+    {
+        return totalResults;
+    }
+
+    public static class FilterPaginator extends AbstractPaginator
+    {
+        private final static long serialVersionUID = 3823389610985272265L;
+
+        private final ExtendedMetadataSearchBean filter;
+
+        public FilterPaginator(ExtendedMetadataSearchBean filter)
+        {
+            super(filter.getResultsPerPage());
+            this.filter = filter;
+        }
+
+        @Override
+        public synchronized List<ResourceDecorator> getCurrentPage()
+        {
+            if(getCurrentPageCache() != null)
+                return getCurrentPageCache();
+
+            List<ResourceDecorator> presults = filter.getResourcesByPage(getPageIndex() + 1);
+            setTotalResults((int) filter.getTotalResultCount());
+
+            setCurrentPageCache(presults);
+
+            return presults;
+        }
+
+    }
+
+    public List<ResourceDecorator> getResourcesByPage(int currentPage)
+    {
+        List<ResourceDecorator> presources = new LinkedList<ResourceDecorator>();
+        List<ResourceDecorator> resources = this.results;
+        int startIndex;
+
+        if(currentPage == 1)
+        {
+            startIndex = 0;
+        }
+        else
+        {
+            startIndex = (currentPage - 1) * this.resultsPerPage;
+        }
+        int endIndex = startIndex + this.resultsPerPage;
+
+        for(int i = 0; i < resources.size(); i++)
+        {
+            if((i > (startIndex - 1)) && (i < endIndex))
+            {
+                presources.add(resources.get(i));
+            }
+        }
+
+        return presources;
+    }
+
+    public ExtendedMetadataSearchFilters getmFilters()
+    {
         return mFilters;
-
     }
 
-    //mtypes getter and setter
-    public List<String> getMtypes()
+    public void setmFilters(ExtendedMetadataSearchFilters mFilters)
     {
-        return mtypes;
+        this.mFilters = mFilters;
     }
 
-    public void setSelectedMtypes(String[] selectedMtypes)
+    public FilterPaginator getFilterPaginator()
     {
-        this.selectedMtypes = selectedMtypes;
+        return filterPaginator;
     }
 
-    public String[] getSelectedMtypes()
+    public void setFilterPaginator(FilterPaginator filterPaginator)
     {
-        return selectedMtypes;
+        this.filterPaginator = filterPaginator;
     }
 
-    //authors getter and setter
-    public String[] getSelectedAuthors()
+    public Integer getResultsPerPage()
     {
-        return selectedAuthors;
+        return resultsPerPage;
     }
 
-    public void setSelectedAuthors(String[] selectedAuthors)
+    public void setResultsPerPage(Integer resultsPerPage)
     {
-        this.selectedAuthors = selectedAuthors;
+        this.resultsPerPage = resultsPerPage;
     }
 
-    public List<String> getAuthors()
+    public Integer getResultsPerGroup()
     {
-        return authors;
+        return resultsPerGroup;
     }
 
-    //sources getter and setter
-    public String[] getSelectedSources()
+    public void setResultsPerGroup(Integer resultsPerGroup)
     {
-        return selectedSources;
+        this.resultsPerGroup = resultsPerGroup;
     }
 
-    public void setSelectedSources(String[] selectedSources)
+    public String getSorting()
     {
-        this.selectedSources = selectedSources;
+        return sorting;
     }
 
-    public List<String> getSources()
+    public void setSorting(String sorting)
     {
-        return sources;
+        this.sorting = sorting;
     }
 
-    //targets getter and setter
-    public String[] getSelectedTargets()
+    public String getGroupField()
     {
-        return selectedTargets;
+        return groupField;
     }
 
-    public void setSelectedTargets(String[] selectedTargets)
+    public void setGroupField(String groupField)
     {
-        this.selectedTargets = selectedTargets;
+        this.groupField = groupField;
     }
 
-    public List<String> getTargets()
+    public long getTotalResults()
     {
-        return targets;
+        return totalResults;
     }
 
-    //purposes getter and setter
-    public String[] getSelectedPurposes()
+    public void setTotalResults(long totalResults)
     {
-        return selectedPurposes;
+        this.totalResults = totalResults;
     }
 
-    public void setSelectedPurposes(String[] selectedPurposes)
+    public int getUserId()
     {
-        this.selectedPurposes = selectedPurposes;
+        return userId;
     }
 
-    public List<String> getPurposes()
+    public void setUserId(int userId)
     {
-        return purposes;
+        this.userId = userId;
     }
 
-    //languages getter and setter
-    public String[] getSelectedLanguages()
+    public void setSort(String sort)
     {
-        return selectedLanguages;
+        this.sorting = sort;
     }
 
-    public void setSelectedLanguages(String[] selectedLanguages)
+    public List<ResourceDecorator> getResults()
     {
-        this.selectedLanguages = selectedLanguages;
+        return results;
     }
 
-    public List<String> getLangs()
+    public void setResults(List<ResourceDecorator> results)
     {
-        return langs;
-    }
-
-    //levels getter and setter
-    public String[] getSelectedLevels()
-    {
-        return selectedLevels;
-    }
-
-    public void setSelectedLevels(String[] selectedLevels)
-    {
-        this.selectedLevels = selectedLevels;
-    }
-
-    public List<String> getLevels()
-    {
-        return levels;
+        this.results = results;
     }
 
 }
