2726ce3c3d7e98ef4a1ccd0f28ffb9ea9a43b479
Maria Karakulina <karakulina@l3s.de>
2018-05-29 07:51:03 +0000

    Fixed last activities for user
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@5779 422a2c9f-66af-4668-9993-60dc3b0871b8

---
+++ src/de/l3s/learnweb/beans/NewsFeedBean.java
@@ -0,0 +1,68 @@
+package de.l3s.learnweb.beans;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import de.l3s.learnweb.LogEntry;
+import de.l3s.learnweb.LogEntry.Action;
+
+@RequestScoped
+@ManagedBean
+public class NewsFeedBean extends ApplicationBean
+{
+
+    private List<LogEntry> newslist;
+    private int entityId;
+
+    private final static Action[] filter = new Action[] { Action.adding_resource, Action.commenting_resource, Action.edit_resource, Action.deleting_resource, Action.group_adding_document, Action.group_adding_link, Action.group_changing_description, Action.group_changing_leader,
+            Action.group_changing_restriction, Action.group_changing_title, Action.group_creating, Action.group_deleting, Action.group_joining, Action.group_leaving, Action.rating_resource, Action.tagging_resource, Action.thumb_rating_resource,
+            Action.changing_resource,
+            Action.group_removing_resource };
+
+    public NewsFeedBean()
+    {
+        Integer groupId = getParameterInt("group_id");
+        Integer userId = getParameterInt("user_id");
+
+        if(groupId != null)
+            entityId = groupId;
+        else if(userId != null)
+        {
+            entityId = userId;
+        }
+        else
+        {
+            entityId = getUser().getId();
+        }
+    }
+
+    public List<LogEntry> getNewslist()
+    {
+        if(null == newslist)
+        {
+            try
+            {
+                newslist = getLearnweb().getLogsByUser(entityId, filter, 50);
+            }
+            catch(SQLException e)
+            {
+                addFatalMessage(e);
+            }
+        }
+        return newslist;
+    }
+
+    public int getEntityId()
+    {
+        return entityId;
+    }
+
+    public void setEntityId(int groupId)
+    {
+        this.entityId = groupId;
+    }
+
+}
