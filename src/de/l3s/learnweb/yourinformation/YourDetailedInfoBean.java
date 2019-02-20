ec9e88be0b734e5534e936691b64d899abe83b80
Yegor Trembovetsky <trembovetsky@l3s.de>
2019-02-20 21:06:44 +0000

    Personal info section updates
    Web page:
    - Create personal info layout
    - Increase amount of displayed data
    
    Java bean:
    - Refactor constructor to reuse User object and throw SQLException
    - Add new fields
    
    Other:
    - Add background image for personal info page
    
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@6139 422a2c9f-66af-4668-9993-60dc3b0871b8

--- src/de/l3s/learnweb/yourinformation/YourDetailedInfoBean.java
+++ src/de/l3s/learnweb/yourinformation/YourDetailedInfoBean.java
@@ -7,6 +7,7 @@ import org.apache.log4j.Logger;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.sql.SQLException;
 import java.util.Date;
 
 /**
@@ -19,45 +20,64 @@ public class YourDetailedInfoBean extends ApplicationBean implements Serializabl
     private static final long serialVersionUID = 6016324259224515500L;
     private static final Logger log = Logger.getLogger(YourCoursesBean.class);
 
-
     private String fullName;
     private String address;
     private String dateOfBirth;
     private String studentId;
+    private String userImage;
+    private String userOrganisation;
+    private String userProfession;
 
-    public YourDetailedInfoBean()
+    public YourDetailedInfoBean() throws SQLException
     {
         User user = getUser();
         if(null == user)
             // when not logged in
             return;
 
-        this.fullName = this.getUser().getFullName();
+        this.fullName = user.getFullName();
         if(null == fullName)
         {
             this.fullName = "N/A";
         }
 
-        this.address = this.getUser().getAddress();
+        this.address = user.getAddress();
         if(null == address)
         {
             this.address = "N/A";
         }
 
-        Date birthDate = this.getUser().getDateOfBirth();
+        Date birthDate = user.getDateOfBirth();
         if(null != birthDate)
         {
-            this.dateOfBirth = this.getUser().getDateOfBirth().toString();
+            this.dateOfBirth = user.getDateOfBirth().toString();
         }
         else
         {
             this.dateOfBirth = "N/A";
         }
 
-        this.studentId = this.getUser().getStudentId();
+        this.studentId = user.getStudentId();
         if(null == studentId)
         {
             this.studentId = "N/A";
+        }
+
+        this.userImage = user.getImage();
+        if(null == userImage){
+            this.userImage = "no_profile.jpg";
+        }
+
+        this.userOrganisation = user.getOrganisation().getTitle();
+        if(null == userOrganisation)
+        {
+            this.userOrganisation = "N/A";
+        }
+
+        this.userProfession = user.getProfession();
+        if(null == userProfession || userProfession.equals(""))
+        {
+            this.userProfession = "N/A";
         }
     }
 
@@ -84,5 +104,19 @@ public class YourDetailedInfoBean extends ApplicationBean implements Serializabl
     public String getStudentId()
     {
         return this.studentId;
+    }
+
+    public String getUserImage(){
+        return this.userImage;
+    }
+
+    public String getUserOrganisation()
+    {
+        return userOrganisation;
+    }
+
+    public String getUserProfession()
+    {
+        return userProfession;
     }
 }
