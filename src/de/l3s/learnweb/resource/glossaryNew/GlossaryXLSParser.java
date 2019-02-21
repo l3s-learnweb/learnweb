c835d4f98439e9ac14a2239c503f9fe85468a379
Kateryna Petukhova <petukhova@l3s.de>
2019-02-21 11:55:13 +0000

    Rewrite parseGlossaryEntries() method
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@6141 422a2c9f-66af-4668-9993-60dc3b0871b8

--- src/de/l3s/learnweb/resource/glossaryNew/GlossaryXLSParser.java
+++ src/de/l3s/learnweb/resource/glossaryNew/GlossaryXLSParser.java
@@ -43,58 +43,70 @@ public class GlossaryXLSParser
             HSSFWorkbook wb = new HSSFWorkbook(fs);
             HSSFSheet sheet = wb.getSheetAt(0);
             HSSFRow row;
-            //HSSFCell cell;
 
             int amountOfRows;
-            amountOfRows = sheet.getPhysicalNumberOfRows() - 1;//-1 because of hedaer
+            amountOfRows = sheet.getPhysicalNumberOfRows() - 1;//-1 because of header
 
-            // int amountOfColumns = 11;
-
-            GlossaryEntry glossaryEntry = new GlossaryEntry();
+            GlossaryEntry currentGlossaryEntry = new GlossaryEntry();
+            List<GlossaryTerm> terms = new LinkedList<>();
 
             for(int r = 1; r < amountOfRows; r++)
             {
                 row = sheet.getRow(r);
                 if(row != null)
                 {
-                    glossaryEntry.setTopicOne(row.getCell(0).getStringCellValue());
-                    glossaryEntry.setTopicTwo(row.getCell(1).getStringCellValue());
-                    glossaryEntry.setTopicThree(row.getCell(2).getStringCellValue());
-                    glossaryEntry.setDescription(row.getCell(3).getStringCellValue());
+                    String topic1 = row.getCell(0).getStringCellValue();
+                    String topic2 = row.getCell(1).getStringCellValue();
+                    String topic3 = row.getCell(2).getStringCellValue();
+                    String description = row.getCell(3).getStringCellValue();
 
-                    List<GlossaryTerm> terms = new LinkedList<>();
-                    for(int i = 1; i < amountOfRows; i++)
+                    if(topic1 == currentGlossaryEntry.getTopicOne() & topic2 == currentGlossaryEntry.getTopicTwo() &
+                            topic3 == currentGlossaryEntry.getTopicThree() & description == currentGlossaryEntry.getDescription())
                     {
-                        String topic1 = row.getCell(0).getStringCellValue();
-                        String topic2 = row.getCell(1).getStringCellValue();
-                        String topic3 = row.getCell(2).getStringCellValue();
-                        String description = row.getCell(3).getStringCellValue();
-                        if(topic1 == glossaryEntry.getTopicOne() & topic2 == glossaryEntry.getTopicTwo() &
-                                topic3 == glossaryEntry.getTopicThree() & description == glossaryEntry.getDescription())
-                        {
-                            GlossaryTerm newTerm = new GlossaryTerm();
-                            newTerm.setTerm(row.getCell(4).getStringCellValue());
-                            //TODO: change language setting below
-                            // newTerm.setLanguage(newTerm.stringToLocaleLanguage(row.getCell(4).getStringCellValue()));
-                            String usesString = row.getCell(6).getStringCellValue();
-                            List<String> uses = Arrays.asList(usesString.split("\\s*,\\s*"));
-                            ;
-                            newTerm.setUses(uses);
-                            newTerm.setPronounciation(row.getCell(7).getStringCellValue());
-                            newTerm.setAcronym(row.getCell(8).getStringCellValue());
-                            newTerm.setSource(row.getCell(9).getStringCellValue());
-                            newTerm.setPhraseology(row.getCell(10).getStringCellValue());
+                        GlossaryTerm newTerm = new GlossaryTerm();
+                        newTerm.setTerm(row.getCell(4).getStringCellValue());
+                        newTerm.setLanguage(languageMap.get(row.getCell(4).getStringCellValue()));
+                        String usesString = row.getCell(6).getStringCellValue();
+                        List<String> uses = Arrays.asList(usesString.split("\\s*,\\s*"));
+                        newTerm.setUses(uses);
+                        newTerm.setPronounciation(row.getCell(7).getStringCellValue());
+                        newTerm.setAcronym(row.getCell(8).getStringCellValue());
+                        newTerm.setSource(row.getCell(9).getStringCellValue());
+                        newTerm.setPhraseology(row.getCell(10).getStringCellValue());
 
-                            terms.add(newTerm);
-                        }
+                        terms.add(newTerm);
+                        currentGlossaryEntry.setTerms(terms);
                     }
-                    glossaryEntry.setTerms(terms);
-                }
-                // don't save anything until we are sure that the parser works as expected getLearnweb().getGlossaryManager().saveEntry(formEntry, glossaryResource);
+                    else
+                    {
+                        //here we should save currentGlossaryTerm
 
-                // addMessage(FacesMessage.SEVERITY_INFO, getLocaleMessage("Changes_saved"));
-                // setKeepMessages();
-                // setNewFormEntry();
+                        currentGlossaryEntry.setTopicOne(topic1);
+                        currentGlossaryEntry.setTopicTwo(topic2);
+                        currentGlossaryEntry.setTopicThree(topic3);
+                        currentGlossaryEntry.setDescription(description);
+
+                        GlossaryTerm newTerm = new GlossaryTerm();
+                        newTerm.setTerm(row.getCell(4).getStringCellValue());
+                        newTerm.setLanguage(languageMap.get(row.getCell(4).getStringCellValue()));
+                        String usesString = row.getCell(6).getStringCellValue();
+                        List<String> uses = Arrays.asList(usesString.split("\\s*,\\s*"));
+                        newTerm.setUses(uses);
+                        newTerm.setPronounciation(row.getCell(7).getStringCellValue());
+                        newTerm.setAcronym(row.getCell(8).getStringCellValue());
+                        newTerm.setSource(row.getCell(9).getStringCellValue());
+                        newTerm.setPhraseology(row.getCell(10).getStringCellValue());
+
+                        terms.clear();
+                        terms.add(newTerm);
+                        currentGlossaryEntry.setTerms(terms);
+                    }
+                    // don't save anything until we are sure that the parser works as expected getLearnweb().getGlossaryManager().saveEntry(formEntry, glossaryResource);
+
+                    // addMessage(FacesMessage.SEVERITY_INFO, getLocaleMessage("Changes_saved"));
+                    // setKeepMessages();
+                    // setNewFormEntry();
+                }
             }
         }
         catch(Exception e)
