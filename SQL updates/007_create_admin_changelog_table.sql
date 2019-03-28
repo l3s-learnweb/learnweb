5de6dbfb0fd6e5e72309909a2f2369498598531a
Oleh Astappiev <astappiev@l3s.de>
2019-03-28 13:36:25 +0000

    Update SQL, rename new table
    git-svn-id: https://svn.l3s.uni-hannover.de/learnweb/trunk/LearnwebOER/Learnweb@6187 422a2c9f-66af-4668-9993-60dc3b0871b8

--- SQL updates/007_create_admin_changelog_table.sql
+++ SQL updates/007_create_admin_changelog_table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `admin_changelog` (
-   `changelog_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `lw_news` (
+   `news_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
    `title` VARCHAR(500) NOT NULL,
    `message` VARCHAR(5000) NULL DEFAULT NULL,
    `user_id` INT UNSIGNED NOT NULL,
    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   PRIMARY KEY (`changelog_id`)
+   PRIMARY KEY (`news_id`)
 ) COLLATE='utf8mb4_general_ci';
