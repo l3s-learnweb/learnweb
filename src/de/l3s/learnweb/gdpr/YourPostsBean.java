1225f570960ff33543e93aa9c5634aad1e8bf6b8
Oleh Astappiev <astappiev@gmail.com>
2019-07-08 14:16:26 +0200

    Add message for an error in YourPostsBean (to help investigate)

--- src/de/l3s/learnweb/gdpr/YourPostsBean.java
+++ src/de/l3s/learnweb/gdpr/YourPostsBean.java
@@ -10,6 +10,7 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import org.jsoup.Jsoup;
+import org.apache.log4j.Logger;
 
 import de.l3s.learnweb.beans.ApplicationBean;
 import de.l3s.learnweb.forum.ForumManager;
@@ -24,7 +25,7 @@ import de.l3s.learnweb.user.User;
 public class YourPostsBean extends ApplicationBean implements Serializable
 {
     private static final long serialVersionUID = 4437146430672930717L;
-    //private static final Logger log = Logger.getLogger(YourPostsBean.class);
+    private static final Logger log = Logger.getLogger(YourPostsBean.class);
 
     private List<ForumPost> userPosts;
     private Map<Integer, String> postThreadTopics;
@@ -37,13 +38,18 @@ public class YourPostsBean extends ApplicationBean implements Serializable
             return;
 
         final ForumManager forumManager = this.getLearnweb().getForumManager();
-        postThreadTopics = new HashMap<>();
 
-        this.userPosts = user.getForumPosts();
+        postThreadTopics = new HashMap<>();
+        userPosts = user.getForumPosts();
+
         for(ForumPost post : userPosts)
         {
-            post.setText(Jsoup.parse(post.getText()).text());
-            postThreadTopics.put(post.getTopicId(), forumManager.getTopicById(post.getTopicId()).getTitle());
+            try {
+                post.setText(Jsoup.parse(post.getText()).text());
+                postThreadTopics.put(post.getTopicId(), forumManager.getTopicById(post.getTopicId()).getTitle());
+            } catch(Exception e) {
+                log.error("An error occurred during processing post " + post.getId(), e);
+            }
         }
     }
 
