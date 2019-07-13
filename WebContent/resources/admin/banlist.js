8e0a734aa4a12a5572225163387152cf52c2fb1a
Oleh Astappiev <astappiev@gmail.com>
2019-07-14 00:56:31 +0200

    Move separated css files to sections of layout scss

--- WebContent/resources/admin/banlist.js
+++
@@ -1,7 +0,0 @@
-function setname(name) {
-	formObj = document.getElementById("banformdialog");
-	nameObj = formObj.elements.item(0);
-	nameObj.value = name;
-	radioObj = formObj.elements.item(0);
-	radioObj.checked = true;
-}
