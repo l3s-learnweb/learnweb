fa311d1d4e12644dae59b3181d7320567f0237f7
Philipp <kemkes@l3s.de>
2020-03-12 12:19:39 +0100

    fix glossary import bug

--- src/de/l3s/learnweb/resource/glossary/GlossaryBean.java
+++ src/de/l3s/learnweb/resource/glossary/GlossaryBean.java
@@ -454,7 +454,7 @@ public class GlossaryBean extends ApplicationBean implements Serializable
         if(overwriteGlossary)
         {
             log.debug("overrideGlossary is true");
-            // delete previous entries, if not a moderator show an error
+            // TODO delete previous entries, if not a moderator show an error
         }
         else
         {
@@ -594,7 +594,7 @@ public class GlossaryBean extends ApplicationBean implements Serializable
 
         //set color and other parameters
         /*Color background = new Color(1f, 1f, 1f, 0.0f);
-        
+
         graphic.setColor(background);
         graphic.setBackground(background);*/
         graphic.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR));
@@ -698,56 +698,56 @@ public class GlossaryBean extends ApplicationBean implements Serializable
     public List<ColumnModel> getColumns()
     {
         List<ColumnModel> columns = new ArrayList<>();
-    
+
         columns.add(new ColumnModel("uses", "uses"));
         columns.add(new ColumnModel("Pronunciation", "pronounciation"));
         columns.add(new ColumnModel("uses", "source"));
         columns.add(new ColumnModel("uses", "phraseology"));
-    
+
         return columns;
     }
-    
-    
+
+
     private void createDynamicColumns() {
         String[] columnKeys = columnTemplate.split(" ");
         columns = new ArrayList<ColumnModel>();
-    
+
         for(String columnKey : columnKeys) {
             String key = columnKey.trim();
-    
+
             if(VALID_COLUMN_KEYS.contains(key)) {
                 columns.add(new ColumnModel(columnKey.toUpperCase(), columnKey));
             }
         }
     }
-    
+
     public void updateColumns()
     {
         //reset table state
         UIComponent table = FacesContext.getCurrentInstance().getViewRoot().findComponent(":form:cars");
         table.setValueExpression("sortBy", null);
-    
+
         //update columns
         createDynamicColumns();
     }
-    
+
     static public class ColumnModel implements Serializable
     {
-    
+
         private String header;
         private String property;
-    
+
         public ColumnModel(String header, String property)
         {
             this.header = header;
             this.property = property;
         }
-    
+
         public String getHeader()
         {
             return header;
         }
-    
+
         public String getProperty()
         {
             return property;
