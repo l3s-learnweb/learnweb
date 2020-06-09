28b38fe04bc0aa3424cdfa8242cf6514d8723a13
Oleh Astappiev <astappiev@l3s.de>
2020-06-09 15:07:12 +0200

    feat: show lifecycle while JSF in DevelopmentStage for any user

--- src/de/l3s/learnweb/beans/LearnwebBean.java
+++ src/de/l3s/learnweb/beans/LearnwebBean.java
@@ -10,6 +10,7 @@ import javax.inject.Named;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.omnifaces.util.Faces;
 
 import de.l3s.learnweb.Learnweb;
 import de.l3s.learnweb.user.User;
@@ -23,6 +24,7 @@ public class LearnwebBean implements Serializable {
 
     private final transient Learnweb learnweb;
     private boolean maintenanceMode = false;
+    private boolean developmentMode = false;
 
     public LearnwebBean() throws ClassNotFoundException, SQLException {
         learnweb = Learnweb.createInstance(BeanHelper.getServerUrl());
@@ -32,6 +34,7 @@ public class LearnwebBean implements Serializable {
     public void init() {
         // initialize stuff which is not required by console tasks
         learnweb.initLearnwebServer();
+        developmentMode = Faces.isDevelopment();
     }
 
     public Learnweb getLearnweb() {
@@ -67,6 +70,10 @@ public class LearnwebBean implements Serializable {
 
     public void setMaintenanceMode(boolean maintenanceMode) {
         this.maintenanceMode = maintenanceMode;
+    }
+
+    public boolean isDevelopmentMode() {
+        return developmentMode;
     }
 
     public String getTrackerApiKey() {
