c82861b8bcb43288a95529ee76a6590fa69ae875
Oleh Astappiev <astappiev@l3s.de>
2021-01-25 09:39:09 +0100

    refactor: update text wanderer

--- WebContent/resources/learnweb/js/jspos/lexer.js
+++ WebContent/resources/learnweb/js/jspos/lexer.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 /*!
  * jsPOS
  *
@@ -8,61 +7,76 @@
  * https://code.google.com/archive/p/jspos/
  */
 
-function LexerNode(string, regex, regexs){
-	this.string = string;
+// eslint-disable-next-line max-classes-per-file
+class LexerNode {
+  /**
+   * @param {string} string
+   * @param {RegExp} regex
+   * @param {RegExp[]} regexs
+   */
+  constructor(string, regex, regexs) {
+    this.string = string;
     this.children = [];
-	if (string) {
-		this.matches = string.match(regex);
-		var childElements = string.split(regex);
-	}
-	if (!this.matches) {
-		this.matches = [];
-		var childElements = [string];
-	}
-	if (regexs.length > 0) {
-        var nextRegex = regexs[0];
-		var nextRegexes = regexs.slice(1);
-		for (var i in childElements) {
-			this.children.push(new LexerNode(childElements[i], nextRegex, nextRegexes));
-        }
-    }
-    else {
-        this.children = childElements;
-    }
-}
 
-LexerNode.prototype.fillArray = function(array){
-    for (var i in this.children) {
-        var child = this.children[i];
-        if (child.fillArray)
-            child.fillArray(array);
-        else if (/[^ \t\n\r]+/i.test(child))
-            array.push(child);
-        if (i < this.matches.length) {
-			var match = this.matches[i];
-			if (/[^ \t\n\r]+/i.test(match))
-                array.push(match);
-        }
+    let childElements;
+    if (string) {
+      this.matches = string.match(regex);
+      childElements = string.split(regex);
     }
-};
 
-LexerNode.prototype.toString = function(){
-    var array = [];
+    if (!this.matches) {
+      this.matches = [];
+      childElements = [string];
+    }
+
+    if (regexs.length > 0) {
+      const nextRegex = regexs[0];
+      const nextRegexes = regexs.slice(1);
+      Array.from(childElements).forEach((element) => {
+        this.children.push(new LexerNode(element, nextRegex, nextRegexes));
+      });
+    } else {
+      this.children = childElements;
+    }
+  }
+
+  fillArray(array) {
+    Array.from(this.children).forEach((child, i) => {
+      if (child.fillArray) {
+        child.fillArray(array);
+      } else if (/[^ \t\n\r]+/i.test(child)) {
+        array.push(child);
+      }
+
+      if (i < this.matches.length) {
+        const match = this.matches[i];
+        if (/[^ \t\n\r]+/i.test(match)) {
+          array.push(match);
+        }
+      }
+    });
+  }
+
+  toString() {
+    const array = [];
     this.fillArray(array);
     return array.toString();
-};
-
-function Lexer(){
-	// Split by numbers, then whitespace, then punctuation
-    this.regexs = [/[0-9]*\.[0-9]+|[0-9]+/ig, /[ \t\n\r]+/ig, /[.,?!]/ig];
+  }
 }
 
-Lexer.prototype.lex = function(string){
-	var array = [];
-    var node = new LexerNode(string, this.regexs[0], this.regexs.slice(1));
+class Lexer {
+  constructor() {
+    // Split by numbers, then whitespace, then punctuation
+    this.regexs = [/[0-9]*\.[0-9]+|[0-9]+/ig, /[ \t\n\r]+/ig, /[.,?!]/ig];
+  }
+
+  lex(string) {
+    const array = [];
+    const node = new LexerNode(string, this.regexs[0], this.regexs.slice(1));
     node.fillArray(array);
     return array;
-};
+  }
+}
 
-//var lexer = new Lexer();
-//print(lexer.lex("I made $5.60 today in 1 hour of work.  The E.M.T.'s were on time, but only barely.").toString());
+// var lexer = new Lexer();
+// console.log(lexer.lex("I made $5.60 today in 1 hour of work.  The E.M.T.'s were on time, but only barely.").toString());
