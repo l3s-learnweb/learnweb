afc621f1d3f028a7a6d51f001cadb7232841412b
Viet Trung Tran <trungtran.utiful@gmail.com>
2023-01-17 13:23:55 +0100

    fix migration syntax

--- src/main/resources/db/test/V1.6__Update_Rdf_table.sql
+++ src/main/resources/db/test/V1.6__Update_Rdf_table.sql
@@ -3,13 +3,13 @@ CREATE TABLE `learnweb_annotations`.`annotation_inputStream` (
     `id` int NOT NULL,
     `user_id` int NOT NULL,
     `type` varchar(250) NOT NULL,
-    `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `content` longtext NOT NULL,
     `date_created` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 ALTER TABLE `learnweb_annotations`.`annotation_inputStream`
     ADD PRIMARY KEY (`id`);
 ALTER TABLE `learnweb_annotations`.`annotation_inputStream`
-    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=247;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 CREATE TABLE IF NOT EXISTS `learnweb_annotations`.`annotation_query_count` (
     `search_id` int NOT NULL,
