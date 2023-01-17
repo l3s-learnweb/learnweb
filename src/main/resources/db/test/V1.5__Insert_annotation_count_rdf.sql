7a7f90b93922919ded99d6e248a3854bb852c3c8
Viet Trung Tran <trungtran.utiful@gmail.com>
2023-01-17 13:23:55 +0100

    fix migration syntax

--- src/main/resources/db/test/V1.5__Insert_annotation_count_rdf.sql
+++ src/main/resources/db/test/V1.5__Insert_annotation_count_rdf.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS `learnweb_annotations`.`annotation_count` (
     `uri_id` int NOT NULL,
     `uri` varchar(250) NOT NULL,
     `users` varchar(250) NOT NULL,
-    `type` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `type` varchar(250) NOT NULL,
     `surface_form` varchar(250) NOT NULL,
-    `session_id` varchar(10000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `session_id` varchar(10000) NOT NULL,
     `confidence` double NOT NULL,
     `input_id` text NOT NULL,
     `created_at` timestamp NOT NULL
@@ -15,12 +15,12 @@ CREATE TABLE IF NOT EXISTS `learnweb_annotations`.`annotation_objects` (
   `user_id` int NOT NULL,
   `group_id` int NOT NULL,
   `application` varchar(250) NOT NULL,
-  `shared_object` varchar(10000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `shared_object` varchar(10000) NOT NULL,
   `created_at` date NOT NULL
 );
 
 CREATE TABLE `learnweb_annotations`.`annotation_rdf` (
   `id` int UNSIGNED NOT NULL,
   `user_id` int NOT NULL,
-  `rdf_value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+  `rdf_value` longtext NOT NULL
 );
