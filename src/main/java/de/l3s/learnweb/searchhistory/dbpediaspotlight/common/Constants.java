5961f73d5583534c5d815e3759c309638ef545df
Oleh Astappiev <astappiev@l3s.de>
2023-03-06 12:13:26 +0100

    chore: fix linter

--- src/main/java/de/l3s/learnweb/searchhistory/dbpediaspotlight/common/Constants.java
+++ src/main/java/de/l3s/learnweb/searchhistory/dbpediaspotlight/common/Constants.java
@@ -2,17 +2,17 @@ package de.l3s.learnweb.searchhistory.dbpediaspotlight.common;
 
 public class Constants {
 
-    public static String EMPTY = "";
+    public static final String EMPTY = "";
 
-    public static String SLASH = "/";
+    public static final String SLASH = "/";
 
-    public static String COMMA = ", ";
+    public static final String COMMA = ", ";
 
-    public static String HTTP = "http";
-    public static String HTTPBIG = "Http";
-    public static String DBPEDIA = "DBpedia:";
+    public static final String HTTP = "http";
+    public static final String HTTPBIG = "Http";
+    public static final String DBPEDIA = "DBpedia:";
 
-    public static String SCHEMA = "Schema:";
+    public static final String SCHEMA = "Schema:";
 
     /**
      * This method is only here to make lint happy.
--- src/main/java/de/l3s/learnweb/searchhistory/dbpediaspotlight/common/Prefixes.java
+++ src/main/java/de/l3s/learnweb/searchhistory/dbpediaspotlight/common/Prefixes.java
@@ -1,8 +1,8 @@
 package de.l3s.learnweb.searchhistory.dbpediaspotlight.common;
 
 public class Prefixes {
-    public static String DBPEDIA_ONTOLOGY = "http://dbpedia.org/ontology/";
-    public static String SCHEMA_ONTOLOGY = "http://schema.org/";
+    public static final String DBPEDIA_ONTOLOGY = "http://dbpedia.org/ontology/";
+    public static final String SCHEMA_ONTOLOGY = "http://schema.org/";
 
     /**
      * This method is only here to make lint happy.
