dae65dd5af64b19addc4817869cd20026cda0ae4
Oleh Astappiev <astappiev@l3s.de>
2024-02-20 17:38:42 +0100

    fix: migrations

--- src/main/resources/db/migration/V17__Modify_survey_detach_from_resource.sql
+++ src/main/resources/db/migration/V17__Modify_survey_detach_from_resource.sql
@@ -1,22 +1,20 @@
 ALTER TABLE `lw_survey_page`
-    CHANGE COLUMN `resource_id` `resource_id` INT(10) UNSIGNED NULL AFTER `page_id`;
+    MODIFY `resource_id` INT(10) UNSIGNED NULL;
+
+ALTER TABLE `lw_survey_question` DROP CONSTRAINT `fk_lw_survey_question_lw_resource`;
+ALTER TABLE `lw_survey_question` DROP COLUMN `resource_id`;
 
 ALTER TABLE `lw_survey_question`
-    DROP COLUMN `resource_id`,
-    DROP INDEX `fk_lw_survey_question_lw_resource`,
-    DROP FOREIGN KEY `fk_lw_survey_question_lw_resource`;
-
-ALTER TABLE `lw_survey_question`
-    CHANGE COLUMN `page_id` `page_id` INT(10) UNSIGNED NOT NULL FIRST;
+    MODIFY `page_id` INT(10) UNSIGNED NOT NULL;
 
 ALTER TABLE `lw_survey_question`
     ADD COLUMN `placeholder` VARCHAR(3000) NULL DEFAULT NULL AFTER `description`;
 
 ALTER TABLE `lw_survey_question_option`
-    CHANGE COLUMN `question_id` `question_id` INT(10) UNSIGNED NOT NULL FIRST;
+    MODIFY `question_id` INT(10) UNSIGNED NOT NULL;
 
 ALTER TABLE `lw_survey_response`
-    CHANGE COLUMN `resource_id` `resource_id` INT(10) UNSIGNED NULL AFTER `response_id`;
+    MODIFY `resource_id` INT(10) UNSIGNED NULL;
 
 ALTER TABLE `lw_survey_response`
     ADD COLUMN `message_id` BIGINT UNSIGNED NULL DEFAULT NULL AFTER `resource_id`;
